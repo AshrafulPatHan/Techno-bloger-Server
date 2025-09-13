4046f9d2ed3021063b81bdad6fdc4a47a8ab7a1c
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-13 12:08:07 +0600

    now i work on new updted vertion

---
+++ DB/_mongodb.js
@@ -0,0 +1,36 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URL
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function connectDB() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        
+        const db = client.db("Pathan-Blogger");
+        return {
+            // Mail: db.collection("Mail"), 
+            User: db.collection('User'),
+            Admin: db.collection('Admin'),
+            Blog: db.collection('Blog'),
+        };
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+  } catch(error) {
+    // Ensures that the client will close when you finish/error
+    console.error("❌ MongoDB connection error:", err);
+    throw err;
+  }
+}
+
+
+module.exports = connectDB;
--- License.md
+++ License.md
@@ -1,1 +1,2 @@
 - website is opean sorce
+- dont hack me
