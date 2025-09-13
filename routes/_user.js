caabca5df124105814190ca488ebdc322ec81ce1
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-13 12:25:47 +0600

    server is straktruly redy

--- routes/_user.js
+++ routes/_user.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const router = express.Router();
 
+module.exports = (collections) => {
+    const { User,Blog } = collections;
+
+// Test the server
+router.get('/test-user',async (req,res) =>{
+    try {
+        const data = "server is ok"
+        res.status(200).send(data)
+    }catch(error){
+        console.error("something is wrong on server",error);
+        res.status(500).send({massage:'Error inserting data'}) 
+    }
+});
+
+return router;
+};
