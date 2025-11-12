e29c36be67256c4eb5a361dfe3bae5a9348d96d7
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-12 15:17:52 +0600

    add comment method

--- src/love.js
+++ src/love.js
@@ -7,7 +7,7 @@ module.exports = (collections) => {
     const { User, Blog } = collections;
 
     // add user watchLists  👨🏽‍🏫
-    router.patch('/post-watchLists', async (req, res) => {
+    router.patch('/post-watchlists', async (req, res) => {
         try {
             // get blog id => id ; and user email => email |
             const { id, email } = req.body;
@@ -23,10 +23,10 @@ module.exports = (collections) => {
 
             // adding watchlist 💝
             const filter = { email: email }; // find the auther
-            const addList = { $push: { watchlists: [id] } }; // 😎 push the objectId for blog inside user collection
+            const addList = { $push: { watchlists: { id } } }; // 😎 push the objectId for blog inside user collection
 
             const result = await User.updateOne(filter, addList);
-            console.log(`💯 A document was inserted with the _id: ${result.insertedId}`);
+            console.log(`💯 WatchLists is add on user collection`);
             res.send(result);
         } catch (error) {
             console.error('⚠️ Error inserting data:', error);
@@ -38,41 +38,40 @@ module.exports = (collections) => {
     router.post('/get-watchlists', async (req, res) => {
         const sendEmail = req.body.email;
         try {
-            // const { email } = req.body;
-            const email = 
+            const { email } = req.body;
+            // const email = "bangladesh@gmail.com";
             if (!email) {
                 return res.status(400).send({ message: "❎ All fields are required" });
             }
             console.log("user email is ", email);
 
-            const find_user = User.findOne({ email: email }); // find the 🧑🏽‍🎤 auther email
-            const lovedBlog = find_user.watchlists;
-            console.log("watchlist data ", lovedBlog); // 💡 see the watchlist data
-            res.send(lovedBlog);
+            const find_user = await User.findOne({ email: email }); // find the 🧑🏽‍🎤 auther email
+            // console.log("user data", find_user); // user data
+
+            const lovedBlog_id = await find_user.watchlists;
+            // console.log("watchlists data ", lovedBlog_id); // 💡 see the watchlist data
+
+            // get all liked blog data
+            const _lovedBlog = await Promise.all(
+                lovedBlog_id.map(async (data) => ({
+                    id_: data.id,
+                    blog: await (async function FindBlog(data) {
+                        let iD = data.id;
+                        let End_result = await Blog.find({ _id: new ObjectId(iD) });
+                        return End_result.toArray();
+                    }(data))
+                }))
+            )
+
+            // console.log("the likes of user : --", _lovedBlog);
+
+            res.send(_lovedBlog);
         } catch (error) {
             console.error('⚠️ Error retrieving data:', error);
             res.status(500).send({ message: `❎ Internal Server Error ${error}` });
         }
     });
 
-    // delet watchlist
-    router.delete('/watchlists/:id', async (req, res) => {
-        const id = req.params.id;
-        try {
-            const result = await wicCollection.deleteOne({ _id: new ObjectId(id) }); // ObjectId ব্যবহার করুন
-            console.log("Delete result:", result);
-
-            if (result.deletedCount === 0) {
-                console.warn("⚠️ Data not found for ID:", id);
-                return res.status(404).send({ message: "❎ Data not found" });
-            }
-
-            res.status(200).send({ message: "✅ Deleted successfully" });
-        } catch (error) {
-            console.error("Error deleting data:", error);
-            res.status(500).send({ message: "❎ Error deleting data" });
-        }
-    });
 
     return router;
 };
